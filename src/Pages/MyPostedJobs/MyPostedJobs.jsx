8dd19f0c78f548764d34cdd15c2475c01ac3da21
seyam <mdseyam3215@gmail.com>
2023-11-06 20:44:31 +0600

    Update page edited

--- src/Pages/MyPostedJobs/MyPostedJobs.jsx
+++ src/Pages/MyPostedJobs/MyPostedJobs.jsx
@@ -11,14 +11,17 @@ const MyPostedJobs = () => {
         Description,
         MaximumPrice,
         MinumumPrice,
+        category,
       }=item;
     return (
         <div>
-        <div className="card lg:card-side bg-sky-300 md:h-[250px] my-4 shadow-xl">
+        <div className="card lg:card-side bg-sky-300 md:h-[350px] my-4 shadow-xl">
          <div className="card-body">
-          <h2 className="card-title "><span className="font-bold ">Name: </span> {jobTitle}</h2>
+          <h2 className="card-title "><span className="font-bold ">jobTitle: </span> {jobTitle}</h2>
           <h2 ><span className="font-bold "> DeadLine:</span> {DeadLine}</h2>
-          <h2><span className="font-bold "> Price range: </span>{MinumumPrice} -{MaximumPrice}</h2>
+          <h2 ><span className="font-bold "> category:</span> {category}</h2>
+          <h2><span className="font-bold "> MinumumPrice: </span>{MinumumPrice} </h2>
+          <h2><span className="font-bold "> MaximumPrice: </span>{MaximumPrice}</h2>
           <h2><span className="font-bold ">Description: </span>{Description}</h2>
           <div className="flex gap-4">
             <Link to={`/updateJobs/${_id}`}>
