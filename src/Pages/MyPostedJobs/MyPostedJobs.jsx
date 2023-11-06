d009b571f3da8a834cbdde4e77e86a62180da30d
seyam <mdseyam3215@gmail.com>
2023-11-06 20:54:10 +0600

    Update page updated

--- src/Pages/MyPostedJobs/MyPostedJobs.jsx
+++ src/Pages/MyPostedJobs/MyPostedJobs.jsx
@@ -11,21 +11,18 @@ const MyPostedJobs = () => {
         Description,
         MaximumPrice,
         MinumumPrice,
-        category,
       }=item;
     return (
         <div>
-        <div className="card lg:card-side bg-sky-300 md:h-[350px] my-4 shadow-xl">
+        <div className="card lg:card-side bg-sky-300 md:h-[250px] my-4 shadow-xl">
          <div className="card-body">
           <h2 className="card-title "><span className="font-bold ">jobTitle: </span> {jobTitle}</h2>
           <h2 ><span className="font-bold "> DeadLine:</span> {DeadLine}</h2>
-          <h2 ><span className="font-bold "> category:</span> {category}</h2>
-          <h2><span className="font-bold "> MinumumPrice: </span>{MinumumPrice} </h2>
-          <h2><span className="font-bold "> MaximumPrice: </span>{MaximumPrice}</h2>
+          <h2><span className="font-bold "> Price range: </span>{MinumumPrice} -{MaximumPrice}</h2>
           <h2><span className="font-bold ">Description: </span>{Description}</h2>
           <div className="flex gap-4">
             <Link to={`/updateJobs/${_id}`}>
-            <button className="btn btn-primary">Update Product</button>
+            <button className="btn btn-primary">Update </button>
             </Link>
             <Link >
             <button className="btn btn-accent w-[153px]">Delete</button>
