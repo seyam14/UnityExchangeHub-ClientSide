583a39eb9ec3b5d00ec9467167bf6d842c1b61eb
seyam <mdseyam3215@gmail.com>
2023-11-08 12:37:52 +0600

    bid request page acpect and reject conditon applied sucessful

--- src/BidRequestsCard/BidRequestsCard.jsx
+++ src/BidRequestsCard/BidRequestsCard.jsx
@@ -1,42 +1,63 @@
 import axios from "axios";
+import  { useState } from "react";
 
+const BidRequestsCard = ({ bid }) => {
+  const { _id, JobTitle, Price, Email, DeadLine, status } = bid;
 
-const BidRequestsCard = ({bid}) => {
-    console.log(bid);
-     
-    const { _id,JobTitle,Price, Email, DeadLine, status } = bid;
-     //     
-    const handelAkcept = (id) => {
-        const status = { status:"progress" };
-        axios
-          .patch(`http://localhost:5000/mybids/${id}`, status)
-          .then((response) => console.log(response))
-          .catch((error) => console.log(error));
-      };
-        const handelReject = (id) => {
-        const status = { status:"Reject" };
-        axios
-          .patch(`http://localhost:5000/mybids/${id}`, status)
-          .then((response) => console.log(response))
-          .catch((error) => console.log(error));
-      };
-    
-    return (
-        <tr>
-        <td>
-            {JobTitle}
-        </td>
-        <td>{Price}</td>
-        <td>{Email}</td>
-        <td>{DeadLine}</td>
-        <td>{status}</td>
-        <td>
-            <button onClick={() =>handelAkcept(_id)} className="btn btn-secondary">Accept</button>
-        </td>
-        <td>
-            <button onClick={() =>handelReject(_id)} className="btn btn-accent">Reject</button>
-        </td>
+  const [currentStatus, setCurrentStatus] = useState(status);
+
+  const handleAccept = (id) => {
+    const newStatus = "Progress";
+    updateBidStatus(id, newStatus);
+    setCurrentStatus(newStatus);
+  };
+
+  const handleReject = (id) => {
+    const newStatus = "Rejected";
+    updateBidStatus(id, newStatus);
+    setCurrentStatus(newStatus);
+  };
+
+  const updateBidStatus = (id, newStatus) => {
+    axios
+      .patch(`http://localhost:5000/mybids/${id}`, { status: newStatus })
+      .then((response) => console.log(response))
+      .catch((error) => console.log(error));
+  };
+
+  return (
+    <tr>
+      <td>{JobTitle}</td>
+      <td>{Price}</td>
+      <td>{Email}</td>
+      <td>{DeadLine}</td>
+      <td>{currentStatus}</td>
+      <td>
+        {currentStatus === "Pending" && (
+          <button onClick={() => handleAccept(_id)} className="btn btn-secondary">
+            Accept
+          </button>
+        )}
+      </td>
+      <td>
+        {currentStatus === "Pending" && (
+          <button onClick={() => handleReject(_id)} className="btn btn-accent">
+            Reject
+          </button>
+        )}
+      </td>
+      <td>
+        {currentStatus !== "Pending" && (
+          <div className="progress-bar">
+            {/* Render the progress bar with the current status */}
+            <div className="progress" style={{ width: "70%" }}>
+              {currentStatus}
+            </div>
+          </div>
+        )}
+      </td>
     </tr>
-);
+  );
 };
+
 export default BidRequestsCard;
